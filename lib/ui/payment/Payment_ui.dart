b99065bece77ccd02e55111d1c241f16058c56ac
[harshabhadra] <[harshasharkey@gmail.com]>
2020-11-24 14:34:52 +0530

    bug fixes

--- lib/ui/payment/Payment_ui.dart
+++ lib/ui/payment/Payment_ui.dart
@@ -66,7 +66,7 @@ class _PaymentUiState extends State<PaymentUi> {
 
   void openCheckout() async {
     var options = {
-      'key': 'rzp_test_nsF8Nu1mu2MKaT',
+      'key': '',
       'amount': 100,
       'name': 'Acme Corp.',
       'description': 'Fine T-Shirt',
